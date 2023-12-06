93ac406f87681846c7223595534c54657b806092
coleYab <yabume123@gmail.com>
2023-12-06 17:12:16 +0300

    This

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -1,5 +1,7 @@
 #!/bin/python3
 def best_score(a_dictionary):
+    if a_dictionary == None or a_dictionary == {}:
+        return None
     maximum = -1213412
     for i, value in a_dictionary.items():
         if maximum < value:
