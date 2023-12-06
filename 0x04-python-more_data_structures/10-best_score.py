9dec00b567ab847e7226613a393fb8dc7448b3a1
coleYab <yabume123@gmail.com>
2023-12-06 15:00:23 +0300

    Data Structre and alghoritm

---
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -0,0 +1,7 @@
+#!/bin/pyhton3
+def best_score(a_dictionary):
+    maximum = -1213412
+    for i, value in a_dictionary.items():
+        if maximum < value:
+            maximum = value
+    return maximum
