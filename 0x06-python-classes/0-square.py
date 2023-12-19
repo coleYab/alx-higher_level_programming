36e3c209e23faeffd0c6e726b0807bea32221b8c
coleYab <yabume123@gmail.com>
2023-12-19 14:23:47 +0300

    oop

--- 0x06-python-classes/0-square.py
+++ 0x06-python-classes/0-square.py
@@ -1,7 +1,8 @@
 #!usr/bin/python3
-"""Defining a class named a square"""
+"""Class module"""
 
 
 class Square:
-    """ A class to abstract the sqare data type """
+    """A class to abstract the sqare data type."""
+
     pass
