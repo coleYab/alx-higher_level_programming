25f678b00df77c6432e6f5e8cac980d23528c71d
coleYab <yabume123@gamil.com>
2024-01-04 16:09:19 +0300

    typo fixed

--- 0x08-python-more_classes/8-rectangle.py
+++ 0x08-python-more_classes/8-rectangle.py
@@ -68,10 +68,9 @@ class Rectangle:
         Rectangle.number_of_instances -= 1
         print('Bye rectangle...')
 
-    @staticmethod
     def bigger_or_equal(rect_1, rect_2):
         if not isinstance(rect_1, Rectangle):
-            raise TypeError("rect_1 must be an integer")
+            raise TypeError("rect_1 must be an instance of Rectangle")
         if not isinstance(rect_2, Rectangle):
-            raise TypeError("rect_2 must be a type of Rectangle")
+            raise TypeError("rect_2 must be an instance of Rectangle")
         return rect_1 if rect_1.area() >= rect_2.area() else rect_2
