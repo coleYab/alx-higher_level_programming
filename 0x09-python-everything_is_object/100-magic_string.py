cbf45900e05b47aba218ac8311ad271dfbacf651
coleYab <yabume123@gamil.com>
2024-01-05 13:34:11 +0300

    .;?,">>>? :: ) :( "

--- 0x09-python-everything_is_object/100-magic_string.py
+++ 0x09-python-everything_is_object/100-magic_string.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
-def magic_string(n):
-    for i in range(n):
-        print('Betty School, ' * i + 'Betty School', sep='')
+def magic_string():
+    magic_string.counter = getattr(magic_string, 'counter', 0) + 1
+    return ('Betty School, ' * (magic_string.counter - 1)) + 'Betty School'
