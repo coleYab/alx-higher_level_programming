f53289d3f4ee7146f7d8ead531c949d3fe202be4
coleYab <yabume123@gamil.com>
2024-01-08 18:14:07 +0300

    Added 101

--- 0x0A-python-inheritance/101-add_attribute.py
+++ 0x0A-python-inheritance/101-add_attribute.py
@@ -5,10 +5,7 @@
 def add_attribute(a_class, name, value):
     """Adds an attribute to an element"""
     if '__slots__' in dir(a_class):
-        if name not in a_class.__slots__:
-            setattr(a_class, name, value)
-        else:
-            raise TypeError("can't add new element")
+        raise TypeError("can't add new element")
     elif name not in dir(a_class):
         setattr(a_class, name, value)
     else:
