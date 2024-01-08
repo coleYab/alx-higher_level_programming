3d42200a907bcd42e728d5226e1d85da7c6b2e86
coleYab <yabume123@gamil.com>
2024-01-08 15:16:30 +0300

    Added 2-is_same_class

--- 0x0A-python-inheritance/2-is_same_class.py
+++ 0x0A-python-inheritance/2-is_same_class.py
@@ -7,7 +7,6 @@ a member of a specific class
 def is_same_class(obj, a_class):
     """Checks if the object 'obj' is in class 'a_class'
     """
-    
+
     temp = a_class()
-    
     return (type(obj) == type(temp))
