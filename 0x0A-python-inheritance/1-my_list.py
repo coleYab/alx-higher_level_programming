4a77e02991129c304c98873427e61fc081ac0b15
coleYab <yabume123@gamil.com>
2024-01-08 14:55:55 +0300

    Added task 2

---
+++ 0x0A-python-inheritance/1-my_list.py
@@ -0,0 +1,15 @@
+#!usr/bin/python3
+"""a module to do some task
+"""
+
+
+class MyList(list):
+    """MyList -> a class that inherits form a list
+    This is he class to do the task
+    """
+    
+    def __init__(self):
+        super(list).__init__(self)
+    
+    def print_sorted(self):
+        print(sorted(self))
