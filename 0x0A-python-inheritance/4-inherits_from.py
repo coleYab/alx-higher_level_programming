f0ef1cf5d4abae0cf1ab2a5c7f47eb3613c91523
coleYab <yabume123@gamil.com>
2024-01-08 15:42:39 +0300

    fixed typos

--- 0x0A-python-inheritance/4-inherits_from.py
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -2,8 +2,9 @@
 """this is a function that will check if a class inherits form other
 """
 
+
 def inherits_from(obj, a_class):
     """iherits from - checks if a class inherits from other class"""
     temp = a_class()
 
-    return (isinstance(obj, a_class) and  type(obj) != type(temp))
+    return (isinstance(obj, a_class) and type(obj) != type(temp))
