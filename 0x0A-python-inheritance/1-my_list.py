98de1d97de5204461f383d2e8a3a6fc5342155a5
coleYab <yabume123@gamil.com>
2024-01-08 19:03:21 +0300

    Added files

--- 0x0A-python-inheritance/1-my_list.py
+++ 0x0A-python-inheritance/1-my_list.py
@@ -9,7 +9,9 @@ class MyList(list):
     """
 
     def __init__(self):
+        """initializes a given list"""
         super().__init__(self)
 
     def print_sorted(self):
+        """prints the sorted version of the list"""
         print(sorted(self))
