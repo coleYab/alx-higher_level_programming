d8b924e1371ce7f7047c82a202a8f966b33016b1
coleYab <yabume123@gamil.com>
2024-01-08 15:03:33 +0300

    Added 1-my_list and tests

--- 0x0A-python-inheritance/1-my_list.py
+++ 0x0A-python-inheritance/1-my_list.py
@@ -7,9 +7,9 @@ class MyList(list):
     """MyList -> a class that inherits form a list
     This is he class to do the task
     """
-    
+
     def __init__(self):
-        super(list).__init__(self)
-    
+        super().__init__(self)
+
     def print_sorted(self):
         print(sorted(self))
