c8dc48e54d19566f5ea01b1673a7a62b2c426095
coleYab <yabume123@gamil.com>
2024-01-08 17:33:08 +0300

    Added Square

--- 0x0A-python-inheritance/10-square.py
+++ 0x0A-python-inheritance/10-square.py
@@ -54,6 +54,7 @@ class Square(Rectangle):
             self.integer_validator("size", size)
         except Exception as e:
             raise e.__class__(e)
+        super().__init__(size, size)
         self.__size = size
 
     def area(self):
