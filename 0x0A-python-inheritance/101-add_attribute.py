a49f0703e06c1c2f92f9ece82e9ee059a8b72c75
coleYab <yabume123@gamil.com>
2024-01-08 18:40:09 +0300

    Added some files:

--- 0x0A-python-inheritance/101-add_attribute.py
+++ 0x0A-python-inheritance/101-add_attribute.py
@@ -4,6 +4,7 @@
 
 def add_attribute(a_class, name, value):
     """Adds an attribute to an element"""
+
     if not hasattr(a_class, '__dict__'):
         raise TypeError("can't add new element")
     setattr(a_class, name, value)
