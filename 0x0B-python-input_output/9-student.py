a90911bef5b35515c3254d9cfda3021cf51f8e1d
coleYab <yabume123@gamil.com>
2024-01-09 19:28:51 +0300

    Adding files

---
+++ 0x0B-python-input_output/9-student.py
@@ -0,0 +1,15 @@
+#!/usr/bin/pyhton3
+"""A function to add a file"""
+
+
+class Student:
+    """A class that abstracts a student to a class"""
+    def __init__(self, first_name, second_name, age):
+        """Constructor"""
+        self.first_name = first_name
+        self.second_name = second_name
+        self.age = age
+
+    def to_json(self):
+        """Returns the dictionary representation of a class"""
+        return self.__dict__
