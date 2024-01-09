c7dca2d57a4f559861c47982ffbc39ac22b09ad7
coleYab <yabume123@gamil.com>
2024-01-09 18:59:25 +0300

    aded files

--- 0x0B-python-input_output/8-class_to_json.py
+++ 0x0B-python-input_output/8-class_to_json.py
@@ -2,10 +2,24 @@
 """A module that convert a class to a string"""
 
 
-import json
-
-
 def class_to_json(a_obj):
     """Converts a class to an object"""
 
-    return json.dumps(a_obj.__dict__)
+    dic = a_obj.__dict__
+
+    string = []
+    for key, value in dic.items():
+        p_h = '"'
+        if not isinstance(value, str):
+            p_h = ''
+        string.append(f'"{key}": {p_h}{value}{p_h}')
+
+    msg = '{'
+    for i in range(len(string) - 1):
+        msg += string[i]
+        msg += ', '
+
+    msg += string[-1]
+    msg += '}'
+
+    return msg
