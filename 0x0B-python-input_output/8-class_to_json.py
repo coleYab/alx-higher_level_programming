0d391aa2ea283dba7ffcc98f1dfc0eb7e1433a52
coleYab <yabume123@gamil.com>
2024-01-09 19:10:25 +0300

    aded files

--- 0x0B-python-input_output/8-class_to_json.py
+++ 0x0B-python-input_output/8-class_to_json.py
@@ -4,19 +4,5 @@
 
 def class_to_json(a_obj):
     """Converts a class to a JSON-formatted string"""
-    def convert_value(value):
-        """Convert non-string values to string representation"""
-        if isinstance(value, str):
-            return f"'{value}'"
-        else:
-            return str(value)
 
-    dic = a_obj.__dict__
-    
-    # Convert key-value pairs to JSON-formatted string
-    json_pairs = [f"'{key}': {convert_value(value)}" for key, value in dic.items()]
-
-    # Construct the final JSON string
-    json_string = "{" + ", ".join(json_pairs) + "}"
-
-    return json_string
+    return a_obj.__dict__
