a8d69142ebe2bab7b3e8e9d2e01e482bd7d1d598
coleYab <yabume123@gamil.com>
2024-01-09 19:44:34 +0300

    added files: :

--- 0x0B-python-input_output/10-student.py
+++ 0x0B-python-input_output/10-student.py
@@ -20,6 +20,18 @@ class Student:
                 if i in dic.keys() and isinstance(i, str):
                     dictt[i] = self.__dict__[i]
 
-                return dictt
+            return dictt
 
         return self.__dict__
+
+
+student_1 = Student("John", "Doe", 23)
+student_2 = Student("Bob", "Dylan", 27)
+
+j_student_1 = student_1.to_json()
+j_student_2 = student_2.to_json(['first_name', 'age'])
+j_student_3 = student_2.to_json(['middle_name', 'age'])
+
+print(j_student_1)
+print(j_student_2)
+print(j_student_3)
