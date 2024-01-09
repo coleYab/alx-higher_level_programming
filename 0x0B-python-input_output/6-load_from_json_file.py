824b93f24edb494165f4b550c4eb25d8bdb2c8f7
coleYab <yabume123@gamil.com>
2024-01-09 17:38:39 +0300

    added files

--- 0x0B-python-input_output/6-load_from_json_file.py
+++ 0x0B-python-input_output/6-load_from_json_file.py
@@ -2,5 +2,11 @@
 """A module to read from a json file"""
 
 
+import json
+
+
 def load_from_json_file(filename):
-    """reads"""
+    """reads the whole files from a file"""
+
+    with open(filename, 'r') as file:
+        return (json.load(file))
