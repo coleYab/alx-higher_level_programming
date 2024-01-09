be8912c47e8191d4709c745503821489b1c5eb1b
coleYab <yabume123@gamil.com>
2024-01-09 20:07:26 +0300

    Added files

---
+++ 0x0B-python-input_output/11-student.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+"""a module to abstract a class student"""
+
+
+class Student:
+    """A class that abstracts a student to a class"""
+    def __init__(self, first_name, second_name, age):
+        """Constructor"""
+        self.first_name = first_name
+        self.last_name = second_name
+        self.age = age
+
+    def to_json(self, attr=None):
+        """Returns the dictionary representation of a class"""
+
+        dic = self.__dict__
+        if isinstance(attr, list):
+            dictt = {}
+            for i in attr:
+                if i in dic.keys() and isinstance(i, str):
+                    dictt[i] = self.__dict__[i]
+
+            return dictt
+
+        return self.__dict__
+
+    def reload_from_json(self, json):
+        """Reloads the file representation of an instance"""
+
+        self.__dict__ = json
