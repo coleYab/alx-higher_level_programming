41910849b898be3d285e88c08259f143c9d95dbc
coleYab <yabume123@gamil.com>
2024-01-09 19:45:15 +0300

    added files: :

--- 0x0B-python-input_output/10-student.py
+++ 0x0B-python-input_output/10-student.py
@@ -23,15 +23,3 @@ class Student:
             return dictt
 
         return self.__dict__
-
-
-student_1 = Student("John", "Doe", 23)
-student_2 = Student("Bob", "Dylan", 27)
-
-j_student_1 = student_1.to_json()
-j_student_2 = student_2.to_json(['first_name', 'age'])
-j_student_3 = student_2.to_json(['middle_name', 'age'])
-
-print(j_student_1)
-print(j_student_2)
-print(j_student_3)
