3a7201064aa41d4f9757da64f62ba49ebe96ea37
coleYab <yabume123@gamil.com>
2024-01-16 04:54:36 +0300

    Added files

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -1,29 +1,18 @@
 #!/usr/bin/python3
-"""
-Adds two numbers and returns the result.
-
-Parameters:
-- num1 (int or float): The first number to be added.
-- num2 (int or float): The second number to be added.
-
-Returns:
-int or float: The sum of num1 and num2.
-"""
-
+'''
+    Class Rectangle
+'''
 from models.base import Base
 
+
 class Rectangle(Base):
+    '''
+        Defining the Rectangle class
+        Inherits from:
+            Base
+    '''
+
     def __init__(self, width, height, x=0, y=0, id=None):
-        """
-            Adds two numbers and returns the result.
-
-        Parameters:
-        - num1 (int or float): The first number to be added.
-        - num2 (int or float): The second number to be added.
-
-        Returns:
-        int or float: The sum of num1 and num2.
-        """
         self.width = width
         self.height = height
         self.x = x
@@ -32,171 +21,120 @@ class Rectangle(Base):
 
     @property
     def width(self):
-        """
-            Adds two numbers and returns the result.
-
-        Parameters:
-        - num1 (int or float): The first number to be added.
-        - num2 (int or float): The second number to be added.
-
-        Returns:
-        int or float: The sum of num1 and num2.
-        """
+        '''
+            Returning private attribute
+        '''
         return self.__width
 
     @width.setter
     def width(self, value):
-        if type(value) != int:
-            raise TypeError("width must be an integer")
-        if value <= 0:
-            raise ValueError("width must be > 0")
+        '''
+            Setting private attribute
+        '''
+        self.setter_validation("width", value)
         self.__width = value
 
     @property
     def height(self):
-        """
-            Adds two numbers and returns the result.
-
-        Parameters:
-        - num1 (int or float): The first number to be added.
-        - num2 (int or float): The second number to be added.
-
-        Returns:
-        int or float: The sum of num1 and num2.
-        """
+        '''
+            Returning private attribute
+        '''
         return self.__height
 
     @height.setter
     def height(self, value):
-        if type(value) != int:
-            raise TypeError("height must be an integer")
-        if value <= 0:
-            raise ValueError("height must be > 0")
+        '''
+            Setting private attribute
+        '''
+        self.setter_validation("height", value)
         self.__height = value
 
     @property
     def x(self):
-        """
-            Adds two numbers and returns the result.
-
-        Parameters:
-        - num1 (int or float): The first number to be added.
-        - num2 (int or float): The second number to be added.
-
-        Returns:
-        int or float: The sum of num1 and num2.
-        """
+        '''
+            Returning private attribute
+        '''
         return self.__x
 
     @x.setter
     def x(self, value):
-        if type(value) != int:
-            raise TypeError("x must be an integer")
-        if value < 0:
-            raise ValueError("x must be >= 0")
+        '''
+            Setting private attribute
+        '''
+        self.setter_validation("x", value)
         self.__x = value
 
     @property
     def y(self):
-        """
-            Adds two numbers and returns the result.
-
-        Parameters:
-        - num1 (int or float): The first number to be added.
-        - num2 (int or float): The second number to be added.
-
-        Returns:
-        int or float: The sum of num1 and num2.
-        """
+        '''
+            Returning private attribute
+        '''
         return self.__y
 
     @y.setter
     def y(self, value):
-        if type(value) != int:
-            raise TypeError("y must be an integer")
-        if value < 0:
-            raise ValueError("y must be >= 0")
+        '''
+            Setting private attribute
+        '''
+        self.setter_validation("y", value)
         self.__y = value
 
     def area(self):
-        """
-            Adds two numbers and returns the result.
-
-        Parameters:
-        - num1 (int or float): The first number to be added.
-        - num2 (int or float): The second number to be added.
-
-        Returns:
-        int or float: The sum of num1 and num2.
-        """
-        return self.__height * self.__width
+        '''
+            Returns the area of the rectangle
+        '''
+        return (self.height * self.width)
 
     def display(self):
-        """
-            Adds two numbers and returns the result.
-
-        Parameters:
-        - num1 (int or float): The first number to be added.
-        - num2 (int or float): The second number to be added.
-
-        Returns:
-        int or float: The sum of num1 and num2.
-        """
-        pre_msg = '\n' * self.__y
-        msg = ''.join((' ' * self.__x) + '#' * self.__width + '\n')
-        print(pre_msg + (msg * self.__height), end='')
-
-    def __str__(self):
-        """
-            Adds two numbers and returns the result.
-
-        Parameters:
-        - num1 (int or float): The first number to be added.
-        - num2 (int or float): The second number to be added.
-
-        Returns:
-        int or float: The sum of num1 and num2.
-        """
-        my_str1 = f"[Rectangle] ({self.id}) {self.__x}/{self.__y}"
-        return (f"{my_str1} - {self.__width}/{self.__height}")
+        '''
+            Prints to stdout the representation of the rectangle
+        '''
+        rectangle = ""
+        print("\n" * self.y, end="")
+        for i in range(self.height):
+            rectangle += (" " * self.x) + ("#" * self.width) + "\n"
+        print(rectangle, end="")
 
     def update(self, *args, **kwargs):
-        """
-            Adds two numbers and returns the result.
-
-        Parameters:
-        - num1 (int or float): The first number to be added.
-        - num2 (int or float): The second number to be added.
-
-        Returns:
-        int or float: The sum of num1 and num2.
-        """
-        if len(args) != 0:
-            try:
-                self.id = args[0]
-                self.width = args[1]
-                self.height = args[2]
-                self.x = args[3]
-                self.y = args[4]
-            except IndexError:
-                pass
-        else:
-            for keys, value in kwargs.items():
-                setattr(self, keys, value)
+        '''
+            Updates the arguments in the class
+        '''
+        if len(args) == 0:
+            for key, val in kwargs.items():
+                self.__setattr__(key, val)
+            return
+        try:
+            self.id = args[0]
+            self.width = args[1]
+            self.height = args[2]
+            self.x = args[3]
+            self.y = args[4]
+        except IndexError:
+            pass
 
     def to_dictionary(self):
-        """
-            Adds two numbers and returns the result.
+        '''
+            Returns a dictionary representation of this class
+        '''
+        return {'x': getattr(self, "x"),
+                'y': getattr(self, "y"),
+                'id': getattr(self, "id"),
+                'height': getattr(self, "height"),
+                'width': getattr(self, "width")}
 
-        Parameters:
-        - num1 (int or float): The first number to be added.
-        - num2 (int or float): The second number to be added.
+    @staticmethod
+    def setter_validation(attribute, value):
+        if type(value) != int:
+            raise TypeError("{} must be an integer".format(attribute))
+        if attribute == "x" or attribute == "y":
+            if value < 0:
+                raise ValueError("{} must be >= 0".format(attribute))
+        elif value <= 0:
+            raise ValueError("{} must be > 0".format(attribute))
 
-        Returns:
-        int or float: The sum of num1 and num2.
-        """
-        return {
-            "id": self.id, "width": self.width,
-            "height": self.height, "x": self.x,
-            "y": self.y
-        }
+    def __str__(self):
+        '''
+            Overwritting the str method
+        '''
+        return "[Rectangle] ({}) {}/{} - {}/{}".format(self.id, self.x, self.y,
+                                                       self.width, self.height)
