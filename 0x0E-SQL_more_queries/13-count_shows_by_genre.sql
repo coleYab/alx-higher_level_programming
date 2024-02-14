cbab88930658c5e079829cdd3251c38c39015e4d
coleYab <yabume123@gmail.com>
2024-02-14 22:46:38 +0300

    finally tires

--- 0x0E-SQL_more_queries/13-count_shows_by_genre.sql
+++
@@ -1,7 +0,0 @@
--- counitng the shows by genre
-SELECT name AS genre, COUNT(show_id) AS number_of_shows
-FROM tv_genres LEFT OUTER JOIN tv_show_genres ON id = genre_id
-GROUP BY name
-WHERE number_of_genres >= 1
-ORDER BY number_of_shows DESC;
-
--- 0x0E-SQL_more_queries/14-my_genres.sql
+++
@@ -1,5 +0,0 @@
--- counitng the shows by genre
-SELECT name FROM tv_shows, tv_show_genres, tv_genres 
-WHERE tv_shows.title = 'Dexter' AND tv_shows.id = show_id AND tv_genres.id = genre_id
-ORDER BY name DESC;
-
