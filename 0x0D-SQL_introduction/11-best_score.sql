e9b0c4d2c81f8fe4cc7370a7add8e06548e81d04
coleYab <yabume123@gamil.com>
2024-02-14 18:40:49 +0300

    added files

--- 0x0D-SQL_introduction/11-best_score.sql
+++ 0x0D-SQL_introduction/11-best_score.sql
@@ -1,3 +1,3 @@
 -- Display the tables that are greater 10
-SELECT score, name FROM second_table WHERE score >= 10 ORDER BY score ASC;
+SELECT score, name FROM second_table WHERE score >= 10 ORDER BY score ASC, name;
 
