4b3a07898d4e1e1bb20e11d4a4475a4b298001ba
coleYab <yabume123@gmail.com>
2024-02-14 22:19:05 +0300

    added files

--- 0x0E-SQL_more_queries/12-no_genre.sql
+++ 0x0E-SQL_more_queries/12-no_genre.sql
@@ -2,6 +2,6 @@
 SELECT title, genre_id
 FROM tv_shows LEFT OUTER JOIN tv_show_genres 
 ON tv_shows.id = tv_show_genres.show_id
-WHERE genre_id IS NULL,
+WHERE genre_id IS NULL
 ORDER BY title ASC, genre_id ASC;
 
