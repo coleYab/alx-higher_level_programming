f6481de5b127df9d24a1459b6c2a3e57b03b8aed
coleYab <yabume123@gmail.com>
2024-02-14 20:30:13 +0300

    added files

--- 0x0E-SQL_more_queries/1-create_user.sql
+++ 0x0E-SQL_more_queries/1-create_user.sql
@@ -1,3 +1,3 @@
 -- create a user in sql
-CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost' IDENTIFIED WITH mysql_native_password BY 'user_0d_1_pwd';
+CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
 
