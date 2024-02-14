717b9c1b3abecfbc7aacc01f5faa32893fa05315
coleYab <yabume123@gamil.com>
2024-02-14 18:33:25 +0300

    added files

--- 0x0D-SQL_introduction/101-avg_temperatures.sql
+++ 0x0D-SQL_introduction/101-avg_temperatures.sql
@@ -1,3 +1,3 @@
 -- Import in hbtn_0c_0 database this table dump
 -- Displays average tempratures
-SELECT city, AVG(values) AS avg_temp FROM temperatures GROUP BY city ORDER BY city, avg_temp DESC;
+SELECT state, AVG(values) AS avg_temp FROM temperatures GROUP BY state ORDER BY state;
