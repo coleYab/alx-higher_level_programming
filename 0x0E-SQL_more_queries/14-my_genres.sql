95da4c3e0590427fac8099e79b7c6d5c5aedb159
coleYab <yabume123@gmail.com>
2024-02-14 22:39:52 +0300

    added files

--- 0x0E-SQL_more_queries/14-my_genres.sql
+++ 0x0E-SQL_more_queries/14-my_genres.sql
@@ -1,5 +1,5 @@
 -- counitng the shows by genre
-SELECT name from tv_shows, tv_show_genres, tv_genres 
+SELECT name FROM tv_shows, tv_show_genres, tv_genres 
 WHERE tv_shows.title = 'Dexter' AND tv_shows.id = show_id AND tv_genres.id = genre_id
 ORDER BY name DESC;
 
