cf513100cf7f61d9f8bbe2603f32561c6717acd7
coleYab <yabume123@gmail.com>
2024-02-14 22:30:34 +0300

    added files

---
+++ 0x0E-SQL_more_queries/13-count_shows_by_genre.sql
@@ -0,0 +1,6 @@
+-- counitng the shows by genre
+SELECT name, COUNT(show_id) AS number_of_shows
+FROM tv_genres LEFT OUTER JOIN tv_show_genres ON id = genre_id
+GROUP BY name
+ORDER BY number_of_shows DESC;
+
