e702506836d661dd23d3fbf4ee04104cf69596f4
coleYab <yabume123@gmail.com>
2024-02-14 21:16:33 +0300

    added files

--- 0x0E-SQL_more_queries/9-cities_by_state_join.sql
+++ 0x0E-SQL_more_queries/9-cities_by_state_join.sql
@@ -1,3 +1,3 @@
 -- using join
-SELECT cities.id, cities.name, state.name FROM cities INNER JOIN ON cities.id = states.id states;
+SELECT cities.id, cities.name, state.name FROM cities INNER JOIN states;
 
