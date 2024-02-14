e69f309b6f940df1b3ee8fe80c21d209cc4a41d1
coleYab <yabume123@gmail.com>
2024-02-14 22:33:57 +0300

    added files

--- 0x0E-SQL_more_queries/13-count_shows_by_genre.sql
+++ 0x0E-SQL_more_queries/13-count_shows_by_genre.sql
@@ -2,5 +2,6 @@
 SELECT name AS genre, COUNT(show_id) AS number_of_shows
 FROM tv_genres LEFT OUTER JOIN tv_show_genres ON id = genre_id
 GROUP BY name
+WHERE number_of_genres >= 1
 ORDER BY number_of_shows DESC;
 
