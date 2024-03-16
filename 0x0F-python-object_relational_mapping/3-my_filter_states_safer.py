9fefa2456c49e9b997ae34132e7b2d693946cd7c
coleYab <yabume123@gamil.com>
2024-03-16 12:42:58 +0300

    Added files

---
+++ 0x0F-python-object_relational_mapping/3-my_filter_states_safer.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""Module to display the files"""
+import MySQLdb
+import sys
+
+if __name__ == '__main__':
+    av = sys.argv
+    lh = "localhost"
+    dab = MySQLdb.connect(
+      host=lh, user=av[1], passwd=av[2], db=av[3], port=3306, charset="utf8"
+    )
+    cusr = dab.cursor()
+    cusr.execute(
+        "SELECT * FROM states WHERE name LIKE '{}' ORDER BY id".format(av[4], )
+        )
+    for row in cusr.fetchall():
+        print(row)
+    cusr.close()
+    dab.close()
