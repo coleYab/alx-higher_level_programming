7105d9824ff1c0bcdfa2177cf58c13bdb4c6df74
coleYab <yabume123@gamil.com>
2024-03-16 11:27:58 +0300

    added files

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -1,13 +1,12 @@
 #!/usr/bin/python3
 """The module to diplay sql statements after filter"""
-
 import MySQLdb
 import sys
 
 if __name__ == '__main__':
     av = sys.argv
     conn = MySQLdb.connect(
-    host="localhost", user=av[1], passwd=av[2], db=av[3], port=3306, charset="utf8"
+    user=av[1], passwd=av[2], db=av[3], port=3306, charset="utf8"
     )
     curser = conn.cursor()
     curser.execute("SELECT * FROM state WHERE name LIKE 'N%' ORDER BY id")
@@ -15,4 +14,3 @@ if __name__ == '__main__':
         print(rows)
     curser.close()
     conn.close()
-
